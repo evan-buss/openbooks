ba3c48bef2c7d76e45daac0234e96a0b4d1c7e9b
Evan Buss <evan.buss28@gmail.com>
2019-10-23 01:58:41 -0400

    Build new binaries

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-env GOOS=windows GOARCH=amd64 go build -o build/OpenBooks_WIN.exe main.go
-env GOOS=darwin GOARCH=amd64 go build -o build/OpenBooks_MAC main.go
-go build -o build/OpenBooks main.go
+packr2
+env GOOS=windows GOARCH=amd64 go build -o build/OpenBooks_WIN.exe
+env GOOS=darwin GOARCH=amd64 go build -o build/OpenBooks_MAC
+env GOOS=linux GOARCH=amd64 go build -o build/OpenBooks_LINUX
+packr2 clean
