183438912080660770191a0cc6929ce2c0be08d3
Evan Buss <evan.buss28@gmail.com>
2019-10-24 23:03:17 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ cd server/app
 npm run build
 cd ../..
 packr2
-env GOOS=windows GOARCH=amd64 go build -o build/OpenBooks_WIN.exe
-env GOOS=darwin GOARCH=amd64 go build -o build/OpenBooks_MAC
-env GOOS=linux GOARCH=amd64 go build -o build/OpenBooks_LINUX
+env GOOS=windows GOARCH=amd64 go build -o build/openbooks.exe
+env GOOS=darwin GOARCH=amd64 go build -o build/openbooks_mac
+env GOOS=linux GOARCH=amd64 go build -o build/openbooks_linux
 packr2 clean
