87e86a1a15ec6286cdd133b06c9ad0e29cf0ef12
robot198 <www.hamdanmahmood12@gmail.com>
2020-10-22 21:02:02 +0800

    Build for PI as well

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ packr2
 env GOOS=windows GOARCH=amd64 go build -o build/openbooks.exe
 env GOOS=darwin GOARCH=amd64 go build -o build/openbooks_mac
 env GOOS=linux GOARCH=amd64 go build -o build/openbooks_linux
+env GOOS=linux GOARCH=arm64 go build -o build/openbooks_linuxPI
 packr2 clean
