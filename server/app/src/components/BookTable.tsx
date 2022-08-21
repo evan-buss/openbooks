16133a869f137bef247fd23a832927c1733a2b20
Evan Buss <evan.buss28@gmail.com>
2022-08-20 23:26:54 -0400

    wip: re-implement table with react-table
    Goals:
    - mantine styling
    - resizeable
    - filterable
    - dark mode

--- server/app/src/components/BookTable.tsx
+++ server/app/src/components/BookTable.tsx
@@ -1,11 +1,15 @@
-import { createStyles, Table } from "@mantine/core";
+import { Box, Button, createStyles, Table } from "@mantine/core";
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  Row,
   useReactTable
 } from "@tanstack/react-table";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef } from "react";
 import { BookDetail } from "../state/messages";
+import "./BookTable.css";
 
 export interface Props {
   books: BookDetail[];
@@ -14,7 +18,9 @@ const columnHelper = createColumnHelper<BookDetail>();
 
 const columns = [
   columnHelper.accessor("server", {
-    header: "Server"
+    header: "Server",
+    minSize: 50,
+    size: 50
   }),
   columnHelper.accessor("author", {
     header: "Author"
@@ -27,67 +33,166 @@ const columns = [
   }),
   columnHelper.accessor("size", {
     header: "Size"
+  }),
+  columnHelper.display({
+    header: "Download",
+    cell: (props) => <Button>Download</Button>
   })
-  //   columnHelper.accessor("full", {
-  //     header: "Status",
-  //   })
-  //   columnHelper.display({
-  //     header: "Download"
-  //   })
 ];
 
 const useStyles = createStyles((theme) => ({
-  row: {
-    padding: theme.spacing.md,
-    margin: theme.spacing.md
-  }
+  container: {
+    border: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[3] : theme.colors.gray[3]
+    }`,
+    borderRadius: theme.radius.md,
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : "white",
+    height: "100%",
+    overflow: "auto",
+    width: "100%"
+  },
+  table: {},
+  head: {
+    position: "sticky",
+    top: 0,
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : "white",
+    zIndex: 1,
+    // Border bottom doesn't work since the table is border-collapse: collapse.
+    boxShadow: `0px 1px 0px 0px ${
+      theme.colorScheme === "dark" ? theme.colors.dark[3] : theme.colors.gray[3]
+    }`
+  },
+  resizer: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    height: "100%",
+    width: "2px",
+    background:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[3]
+        : theme.colors.gray[6],
+    cursor: "col-resize",
+    userSelect: "none",
+    touchAction: "none",
+    opacity: 0,
+
+    ["&.isResizing"]: {
+      background:
+        theme.colorScheme === "dark"
+          ? theme.colors.brand[3]
+          : theme.colors.brand[4],
+      opacity: 1
+    },
+
+    ["&:hover"]: {
+      opacity: 1
+    }
+  },
+  th: {
+    position: "relative"
+  },
+  row: {}
 }));
 
 export default function BookTable({ books: data }: Props) {
-  const { classes } = useStyles();
+  const { classes, cx } = useStyles();
+
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+  const rowVirtualizer = useVirtualizer({
+    count: data.length,
+    getScrollElement: () => tableContainerRef.current,
+    estimateSize: () => 57,
+    overscan: 10
+  });
+
+  const paddingTop =
+    rowVirtualizer.getVirtualItems().length > 0
+      ? rowVirtualizer.getVirtualItems()?.[0]?.start || 0
+      : 0;
+  const paddingBottom =
+    rowVirtualizer.getVirtualItems().length > 0
+      ? rowVirtualizer.getTotalSize() -
+        (rowVirtualizer.getVirtualItems()?.[
+          rowVirtualizer.getVirtualItems().length - 1
+        ]?.end || 0)
+      : 0;
+
   const table = useReactTable({
     data,
     columns: columns,
+    columnResizeMode: "onChange",
     getCoreRowModel: getCoreRowModel()
   });
 
-  const headers = table.getHeaderGroups().map((headerGroup) => (
-    <tr key={headerGroup.id}>
-      {headerGroup.headers.map((header) => (
-        <th key={header.id}>
-          {header.isPlaceholder
-            ? null
-            : flexRender(header.column.columnDef.header, header.getContext())}
-        </th>
-      ))}
-    </tr>
-  ));
-
-  const rows = table.getRowModel().rows.map((row) => (
-    <tr key={row.id}>
-      {row.getVisibleCells().map((cell) => (
-        <td key={cell.id}>
-          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-        </td>
-      ))}
-    </tr>
-  ));
+  const { rows: tableRows } = table.getRowModel();
 
   return (
-    <Table
-      highlightOnHover
-      verticalSpacing="md"
-      sx={(theme) => ({
-        borderCollapse: "separate",
-        borderRadius: theme.radius.md,
-        border: `1px solid ${
-          theme.colorScheme === "dark"
-            ? theme.colors.dark[5]
-            : theme.colors.gray[2]
-        }`
-      })}>
-      <thead style={{ border: "1px solid gray" }}>{headers}</thead>
-      <tbody style={{ border: "1px solid gray" }}>{rows}</tbody>
-    </Table>
+    <Box ref={tableContainerRef} className={classes.container}>
+      <Table highlightOnHover verticalSpacing="xs">
+        <thead className={classes.head}>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th
+                  className={classes.th}
+                  key={header.id}
+                  style={{
+                    width: header.getSize()
+                  }}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+
+                  <div
+                    onMouseDown={header.getResizeHandler()}
+                    onTouchStart={header.getResizeHandler()}
+                    className={cx(classes.resizer, {
+                      ["isResizing"]: header.column.getIsResizing()
+                    })}
+                  />
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody style={{ height: "100px", maxHeight: "100px" }}>
+          {paddingTop > 0 && (
+            <tr>
+              <td style={{ height: `${paddingTop}px` }} />
+            </tr>
+          )}
+          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+            const row = tableRows[
+              virtualRow.index
+            ] as unknown as Row<BookDetail>;
+            return (
+              <tr key={row.id} className={classes.row}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+          {paddingBottom > 0 && (
+            <tr>
+              <td style={{ height: `${paddingBottom}px` }} />
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </Box>
   );
 }
