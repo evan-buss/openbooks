9ec356b640d0bb15ee995b4acf492bd9a511c9ed
Evan Buss <evan.buss28@gmail.com>
2023-01-08 13:15:26 -0500

    fix: version string

--- cmd/openbooks/main.go
+++ cmd/openbooks/main.go
@@ -41,7 +41,7 @@ func init() {
 	desktopCmd.PersistentFlags().BoolVar(&globalFlags.EnableTLS, "tls", true, "Connect to server using TLS.")
 	desktopCmd.PersistentFlags().BoolVarP(&globalFlags.Log, "log", "l", false, "Save raw IRC logs for each client connection.")
 	desktopCmd.PersistentFlags().StringVar(&globalFlags.SearchBot, "searchbot", "search", "The IRC bot that handles search queries. Try 'searchook' if 'search' is down.")
-	desktopCmd.PersistentFlags().StringVarP(&globalFlags.UserAgent, "useragent", "u", fmt.Sprintf("OpenBooks Server %s", ircVersion), "UserAgent / Version Reported to IRC Server.")
+	desktopCmd.PersistentFlags().StringVarP(&globalFlags.UserAgent, "useragent", "u", fmt.Sprintf("OpenBooks %s", ircVersion), "UserAgent / Version Reported to IRC Server.")
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
