32b77f1ff6da76c52dbca4d9a678e3c562a01d8b
Evan Buss <evan.buss28@gmail.com>
2024-05-05 16:06:32 -0400

    fix: remove framer-motion util file (#165)
    - forgot to delete file after removing framer-motion dependency

--- server/app/src/utils/animation.ts
+++
@@ -1,9 +0,0 @@
-import { HTMLMotionProps } from "framer-motion";
-
-export const defaultAnimation: HTMLMotionProps<"div"> = {
-  layout: true,
-  initial: { scale: 0.8, opacity: 0 },
-  animate: { scale: 1, opacity: 1 },
-  exit: { scale: 0.8, opacity: 0 },
-  transition: { type: "tween" }
-};
